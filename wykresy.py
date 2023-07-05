08a918e137677ed0bf2f2d810e30a016e107f50e
sliwk <sliwa.pawel@interia.pl>
2023-07-05 13:44:26 +0200

    6 commit - dodanie pół-automatycznego ścigania danych

--- wykresy.py
+++ wykresy.py
@@ -2,7 +2,40 @@ import PySimpleGUI as sg
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
+import subprocess
+import sys
+import schedule
+import time
+import numpy as np
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import os
+import shutil
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+import win32com.client as win32
+import pandas as pd
+from datetime import datetime, timedelta
+import datetime as dt
+import time
+import numpy as np
+import xlrd
+import urllib.request
+import requests
+import sys
+import getpass
+import json
+import lxml
+import openpyxl
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl import load_workbook
+import holidays
+import holidays.countries
 
 cale=[]
 kwartaly=[]
@@ -63,18 +96,6 @@ def draw_chart(produkt):
     fig.autofmt_xdate(rotation=35, ha='right')    #rotuje daty wyświetlane pod wykresem
     plt.show()
 
-#draw_chart("BASE_Y-25")
-def draw_ratio(produkt):
-    df_temp=df_wsp[df_wsp['kontrakt short']==produkt]
-    data=df_temp['Data']
-    ratio=df_temp['ratio']
-    plt.plot(data, ratio,marker='o', linestyle='-', color='blue')
-    plt.title(produkt)
-    plt.xlabel('Data')
-    plt.ylabel('ratio')
-    plt.show()
-
-#draw_ratio("Y-24")
 
 sg.theme("Black") #gotowe motywy z kolorystyka do podejrzenia w internecie
 
@@ -89,12 +110,14 @@ Q_combo=sg.Combo(kwartaly, font=('Arial Bold', 14),  expand_x=True, enable_event
 M_label=sg.Text("M")
 M_combo=sg.Combo(msc, font=('Arial Bold', 14),  expand_x=True, enable_events=True, key='M_droplist')
 
-
+download_button=sg.Button(button_text="zaciągnij brakujące dane z TGE",key="download")
 
 
 window=sg.Window("Wykresy",
                  layout=[[label],
-                        [Y_label,Y_combo,Q_label,Q_combo,M_label,M_combo]],size=(800,100),element_justification='c')
+                        [Y_label,Y_combo,Q_label,Q_combo,M_label,M_combo],
+                        [download_button]],
+                        size=(800,100),element_justification='c')
 
 
 while True:
@@ -110,4 +133,83 @@ while True:
             draw_chart(values['Q_droplist'])
         case 'M_droplist':
             draw_chart(values['M_droplist'])
+        case 'download':
+            window.Hide()  # ukrywa pierwotne okno Gui(niezaktualizowane)
+            # TUTAJ WKLEJAM CAŁY KOD Z PLIKU MAIN (żeby pyinstaller miał 1 plik do przekompilowania)
+            # znajduje ostatnią uzupełnioną datę w pliku
+            wb = load_workbook(filename="abc.xlsx")
+            ws = wb["a"]
+            ostatni_wiersz = ws.max_row
+            ostatnia_data = ws.cell(row=ostatni_wiersz,
+                                    column=1).value  # uchwycona ostatnia data, dla której są dane w pliku excel
+            pl_holidays = holidays.Poland()
+
+            # kod  na ostatni dzień roboczy
+            dzisiaj = dt.date.today()
+            delta1 = dt.timedelta(days=1)
+            delta2 = dt.timedelta(days=2)
+            ostatni_dzien = dzisiaj - delta1
+
+            if ostatni_dzien.weekday() == 5:
+                ostatni_dzien = ostatni_dzien - delta1
+            elif ostatni_dzien.weekday() == 6:
+                ostatni_dzien = ostatni_dzien - delta2
+
+            ostatni_dzien_str = str(ostatni_dzien)
+
+            for x in range(len(pl_holidays)):
+                if ostatni_dzien_str in pl_holidays:
+                    ostatni_dzien = ostatni_dzien - delta1
+                    if ostatni_dzien.weekday() == 5:
+                        ostatni_dzien = ostatni_dzien - delta1
+                    elif ostatni_dzien.weekday() == 6:
+                        ostatni_dzien = ostatni_dzien - delta2
+                    ostatni_dzien_str = str(ostatni_dzien)
+
+            weekdays = [5, 6]
+            data_poczatkowa = dt.datetime.strptime(ostatnia_data,
+                                                   "%d-%m-%Y") + delta1  # trzeba do ostatniej daty dodać 1 dzień
+            start_day = data_poczatkowa
+            end_day = ostatni_dzien
+
+            daterange = pd.date_range(start_day, end_day)
+            for date in daterange:
+                if date.weekday() not in weekdays and date.strftime("%Y-%m-%d") not in pl_holidays:
+                    dzien = date.strftime("%d-%m-%Y")
+
+                    sciezkaWebDriver = r"C:\Users\psliwa\PycharmProjects\Pobieranie_danych_tge\chromedriver.exe"  # do ściezki doklejam chromedriver, który wczesniej instaluje ze strony (sprawdź wersje chrome i sciągnij odpowiedni chromedriver)
+                    # https://chromedriver.chromium.org/downloads     link do sciągniecia chromedrivera
+                    # ileDni=1
+                    driver = webdriver.Chrome(executable_path=sciezkaWebDriver)
+                    url = 'https://tge.pl/energia-elektryczna-otf?dateShow=' + dzien + '&dateAction=prev'
+                    page = driver.get(url)
+                    time.sleep(1)
+                    # -----BASE ---
+                    df = pd.read_html(driver.page_source, header=0, decimal=",", thousands='.')
+                    df[0] = df[0].drop('Unnamed: 1', axis=1)  # df[0] dla base   df[1] dla peak
+                    base = df[0]
+                    base = base.iloc[:-1]  # usunięcie ostatniego wiersza z tabeli (podsumowania)
+                    base.insert(0, 'data', dzien)  # dodanie daty w pierwszej kolumnie(0-zerowej)
+                    base[
+                        'typ'] = "BASE"  # dodajemy kolumne Typ: "BASE" dla produktów z tabeli base, PEAK dla produktów z tabeli PEAK
+                    # -----PEAK-----
+                    df[1] = df[1].drop('Unnamed: 1', axis=1)
+                    peak = df[1]
+                    peak = peak.iloc[:-1]
+                    peak.insert(0, 'data', dzien)
+                    peak['typ'] = 'PEAK'
+
+                    wb = load_workbook(filename="abc.xlsx")
+                    ws = wb["a"]
+                    for x in dataframe_to_rows(base, index=False, header=False):
+                        ws.append(x)  # append dodaje dane do już istniejących w pliku
+                    wb.save("abc.xlsx")
+                    for x in dataframe_to_rows(peak, index=False, header=False):
+                        ws.append(x)
+                    wb.save("abc.xlsx")
+
+                    driver.close()
+                    driver.quit()
+                    # KONIEC PLIKU MAIN
+            subprocess.Popen([sys.executable, "wykresy.py"])  # uruchamiam GUI jeszcze raz,żeby zaciągniete
 window.close()
